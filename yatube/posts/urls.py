3bb46792560462974f97d41a71a8c3358f0bda8c
Екатерина Алексеенк <katrinale@yandex.ru>
2022-02-17 01:12:06 +0300

    Добавлены urls и view-функции

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -3,8 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
-    #path('admin/', admin.site.urls)
-    #path('posts/<slug:groups>/', views.group_posts())
     path(
         'group/<slug:slug>/',
         views.group_posts
