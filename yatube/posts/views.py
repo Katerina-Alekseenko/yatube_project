0955d1ecbdc600ef3808c768242e2f00ec7868d2
Екатерина Алексеенк <katrinale@yandex.ru>
2022-03-05 06:45:52 +0300

    завершила тему шаблоны и теги Django

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 # Главная страница
 def index(request):
-    return HttpResponse('Главная страница')
+    template = 'posts/index.html'
+    title_index = 'Это главная страница проекта Yatube'
+    context = {'title_index': title_index}
+    return render(request, template, context)
 
 
 def group_posts(request, slug):
-    return HttpResponse(f'Посты, отфильтрованные по группам {slug}')
+    template = 'posts/group_list.html'
+    title_group_list = 'Здесь будет информация о группах проекта Yatube'
+    context = {'title_group_list': title_group_list}
+    return render(request, template, context, slug)
+
+
+def group_list(request, slug):
+    template = 'posts/group_list.html'
+    return render(request, template)
